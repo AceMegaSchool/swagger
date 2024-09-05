2ae2dbe8ebe967a9f62a89448a6681d2cd776bd3
Jerome Bell <jeromebell@gmail.com>
2024-09-05 14:19:57 -0500

    Default API spec file

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://acemegaschool.github.io/evanston/asyncapi.yaml",
+    url: "https://acemegaschool.github.io/evanston/api-spec.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
